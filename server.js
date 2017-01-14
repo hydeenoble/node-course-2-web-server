3d9a55f5f3c78efcb386fa22ebe9f7a376b1d232
Emehinola Idowu <hydeenoble39@gmail.com>
2017-01-14 21:45:54 +0100

    added the projects url

--- server.js
+++ server.js
@@ -56,7 +56,11 @@ app.get('/about', function (req, res) {
     });
 });
 
-
+app.get('/projects', function (req, res) {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page'
+    });
+});
 app.get('/bad', function (req, res) {
     res.send({
         errorMessage: 'Error handling request...'
